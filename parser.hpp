8a84dc022d21ad4b8da231c6c21e61ac0025ec2e
happierpig <zhaoyilong217@sjtu.edu.cn>
2021-07-01 14:26:48 +0800

    bug--

--- parser.hpp
+++ parser.hpp
@@ -177,7 +177,7 @@ public:
 };
 
 bool modifyPc(opClass type){
-    return (type == AUIPC || type == JAL || type == JALR);
+    return (type == AUIPC || type == JAL || type == JALR || type == BEQ || type == BNE || type == BLT || type == BGE || type == BLTU || type == BGEU);
 }
 bool readRAM(opClass type){
     return (type == LB || type == LH || type == LW || type == LBU || type == LHU );
