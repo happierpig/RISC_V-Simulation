b470a6aa5fdc15744973c1776bedff5f7fc3d483
happierpig <zhaoyilong217@sjtu.edu.cn>
2021-07-01 14:54:52 +0800

    Pipeline Accepted

--- program.hpp
+++ program.hpp
@@ -102,6 +102,7 @@ private:
         rde = {rfd.pc,ID_code.getClass(),reg[rs1],reg[rs2],ID_code.getrd(),ID_code.getrs1(),ID_code.getrs2(),ID_code.getShamt(),ID_code.getimm()};
         if(modifyPc(rde.codeClass)){bubbles = 3;} // stop
         if(readRAM(rde.codeClass)){bubbles = 1;} // stop
+        if(checkNoRs(rde.codeClass)) return;
         // forwarding naming short circuit; maybe very fake(todo)
         if(rmw.codeClass != bubble && rmw.codeClass != end && rmw.regFlag && rmw.rd != 0){
             if(checkRs1(rde.codeClass)){
